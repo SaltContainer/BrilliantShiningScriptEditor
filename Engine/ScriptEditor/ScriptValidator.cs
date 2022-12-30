9af0164b6f74cf7a83bfb764e748e137ea1de080
Samuel Lambert <fanmc34@gmail.com>
2022-12-30 02:39:41 -0500

    Working use of flag and work names

--- Engine/ScriptEditor/ScriptValidator.cs
+++ Engine/ScriptEditor/ScriptValidator.cs
@@ -34,13 +34,22 @@ namespace PokemonBDSPEditor.Engine.ScriptEditor
                             convertedArgs.Add(BitConverter.ToSingle(BitConverter.GetBytes(arg.GetNumberValue()), 0).ToString());
                             break;
                         case ArgumentType.Variable:
-                            convertedArgs.Add(string.Format("var_{0}", arg.GetNumberValue()));
+                            convertedArgs.Add(FileConstants.WorkVariables.Where(w => w.Id == arg.GetNumberValue())
+                                .Select(w => w.Name)
+                                .DefaultIfEmpty(string.Format("var_{0}", arg.GetNumberValue()))
+                                .First());
                             break;
                         case ArgumentType.Flag:
-                            convertedArgs.Add(string.Format("flag_{0}", arg.GetNumberValue()));
+                            convertedArgs.Add(FileConstants.Flags.Where(f => f.Id == arg.GetNumberValue())
+                                .Select(f => f.Name)
+                                .DefaultIfEmpty(string.Format("flag_{0}", arg.GetNumberValue()))
+                                .First());
                             break;
                         case ArgumentType.SystemFlag:
-                            convertedArgs.Add(string.Format("sys_{0}", arg.GetNumberValue()));
+                            convertedArgs.Add(FileConstants.SystemFlags.Where(s => s.Id == arg.GetNumberValue())
+                                .Select(s => s.Name)
+                                .DefaultIfEmpty(string.Format("sys_{0}", arg.GetNumberValue()))
+                                .First());
                             break;
                         case ArgumentType.String:
                             convertedArgs.Add(arg.GetStringValue());
@@ -81,9 +90,24 @@ namespace PokemonBDSPEditor.Engine.ScriptEditor
                             byte[] bytes = BitConverter.GetBytes(float.Parse(arg));
                             convertedArguments.Add(new Argument(ArgumentType.Number, BitConverter.ToInt32(bytes, 0)));
                         }
-                        else if (RegexPatterns.RegexVariable.IsMatch(arg)) convertedArguments.Add(new Argument(ArgumentType.Variable, int.Parse(arg.Split('_')[1])));
-                        else if (RegexPatterns.RegexFlag.IsMatch(arg)) convertedArguments.Add(new Argument(ArgumentType.Flag, int.Parse(arg.Split('_')[1])));
-                        else if (RegexPatterns.RegexSystemFlag.IsMatch(arg)) convertedArguments.Add(new Argument(ArgumentType.SystemFlag, int.Parse(arg.Split('_')[1])));
+                        else if (IsWork(arg))
+                        {
+                            ArgumentTypeInfo result = GetWorkFromName(arg);
+                            if (result != null) convertedArguments.Add(new Argument(ArgumentType.Variable, result.Id));
+                            else convertedArguments.Add(new Argument(ArgumentType.Variable, int.Parse(arg.Split('_')[1])));
+                        }
+                        else if (IsFlag(arg))
+                        {
+                            ArgumentTypeInfo result = GetFlagFromName(arg);
+                            if (result != null) convertedArguments.Add(new Argument(ArgumentType.Flag, result.Id));
+                            else convertedArguments.Add(new Argument(ArgumentType.Flag, int.Parse(arg.Split('_')[1])));
+                        }
+                        else if (IsSysFlag(arg))
+                        {
+                            ArgumentTypeInfo result = GetSysFlagFromName(arg);
+                            if (result != null) convertedArguments.Add(new Argument(ArgumentType.SystemFlag, result.Id));
+                            else convertedArguments.Add(new Argument(ArgumentType.SystemFlag, int.Parse(arg.Split('_')[1])));
+                        }
                         else convertedArguments.Add(new Argument(ArgumentType.String, arg));
                     }
                     if (ValidateArguments(convertedArguments, i)) convertedCommands.Add(new Command(convertedArguments));
@@ -156,22 +180,37 @@ namespace PokemonBDSPEditor.Engine.ScriptEditor
 
         private bool ValidateCommand(string command, int line)
         {
-            if (RegexPatterns.RegexValidCommand.IsMatch(command))
+            if (GetCommandFromName(command) != null)
             {
-                CommandInfo foundCommand = GetCommandFromName(command);
-                if (foundCommand == null)
+                return true;
+            }
+            else
+            {
+                if (RegexPatterns.RegexInvalidCommand.IsMatch(command))
+                {
+                    return true;
+                }
+                else
                 {
                     validationExceptions.Add(new ScriptValidationException(string.Format("Line {0}: Invalid command.", line + 1), false));
                     return false;
                 }
             }
-            else if (!RegexPatterns.RegexInvalidCommand.IsMatch(command))
-            {
-                validationExceptions.Add(new ScriptValidationException(string.Format("Line {0}: Invalid command.", line + 1), false));
-                return false;
-            }
+        }
 
-            return true;
+        private bool IsFlag(string command)
+        {
+            return GetFlagFromName(command) != null || RegexPatterns.RegexFlag.IsMatch(command);
+        }
+
+        private bool IsSysFlag(string command)
+        {
+            return GetSysFlagFromName(command) != null || RegexPatterns.RegexSystemFlag.IsMatch(command);
+        }
+
+        private bool IsWork(string command)
+        {
+            return GetWorkFromName(command) != null || RegexPatterns.RegexVariable.IsMatch(command);
         }
 
         private CommandInfo GetCommandFromName(string name)
@@ -184,6 +223,36 @@ namespace PokemonBDSPEditor.Engine.ScriptEditor
             return FileConstants.Commands.Where(c => c.Id == id).DefaultIfEmpty(null).First();
         }
 
+        private ArgumentTypeInfo GetFlagFromName(string name)
+        {
+            return FileConstants.Flags.Where(f => f.Name == name).DefaultIfEmpty(null).First();
+        }
+
+        private ArgumentTypeInfo GetFlagFromId(int id)
+        {
+            return FileConstants.Flags.Where(f => f.Id == id).DefaultIfEmpty(null).First();
+        }
+
+        private ArgumentTypeInfo GetSysFlagFromName(string name)
+        {
+            return FileConstants.SystemFlags.Where(s => s.Name == name).DefaultIfEmpty(null).First();
+        }
+
+        private ArgumentTypeInfo GetSysFlagFromId(int id)
+        {
+            return FileConstants.SystemFlags.Where(s => s.Id == id).DefaultIfEmpty(null).First();
+        }
+
+        private ArgumentTypeInfo GetWorkFromName(string name)
+        {
+            return FileConstants.WorkVariables.Where(w => w.Name == name).DefaultIfEmpty(null).First();
+        }
+
+        private ArgumentTypeInfo GetWorkFromId(int id)
+        {
+            return FileConstants.WorkVariables.Where(w => w.Id == id).DefaultIfEmpty(null).First();
+        }
+
         private string GetTypeNameFromType(ArgumentType type)
         {
             string result = "";
@@ -193,7 +262,7 @@ namespace PokemonBDSPEditor.Engine.ScriptEditor
                     result = "Number";
                     break;
                 case ArgumentType.Variable:
-                    result = "Var";
+                    result = "Work";
                     break;
                 case ArgumentType.Flag:
                     result = "Flag";
